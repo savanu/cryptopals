02db4e68d8d3cf8284121824f4b425d460d27575
Savid Sharan <savidsharan@gmail.com>
2024-06-17 21:23:40 -0400

    Remove rust files

--- src_old/challenge_one.rs
+++
@@ -1,8 +0,0 @@
-pub fn run() {
-    let answer = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    let payload = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let result = crate::utils::hex_to_base64(&payload);
-
-    assert_eq!(answer, result);
-    println!("Challenge 1: {result}");
-}
